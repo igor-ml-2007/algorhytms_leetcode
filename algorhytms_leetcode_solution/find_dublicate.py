c5112c9d2d26b5a7775cff96658f2d61e0b47caf
igor-ml-2007 <157715076+igor-ml-2007@users.noreply.github.com>
2025-09-26 12:39:17 +0300

    Update find_dublicate.py

--- algorhytms_leetcode_solution/find_dublicate.py
+++ algorhytms_leetcode_solution/find_dublicate.py
@@ -1,5 +1,23 @@
+# solution 1
 def find_duplicate(array: list):
     for i in range(len(array)):
         if array[i] == array[i + 1]:
             return array[i]
+
+# solution 2
+def find_duplicate(array):
+    fast, slow = 0, 0
+    while True:
+        slow = array[slow]
+        fast = array[array[fast]]
+        if slow == fast:
+            break
+
+    slow2 = 0
+    while True:
+        slow = array[slow]
+        slow2 = array[slow2]
+        if (slow == slow2):
+            return slow
+
 
