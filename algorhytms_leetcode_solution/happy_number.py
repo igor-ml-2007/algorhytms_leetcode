0e9076846964573a02763ee195ceb7dfecdeb256
igor-ml-2007 <157715076+igor-ml-2007@users.noreply.github.com>
2025-09-26 12:47:00 +0300

    Update happy_number.py

--- algorhytms_leetcode_solution/happy_number.py
+++ algorhytms_leetcode_solution/happy_number.py
@@ -1,3 +1,4 @@
+# solution 1
 def is_happy(n):
     if len(str(n)) == 1:
         return n == 1
@@ -6,3 +7,24 @@ def is_happy(n):
         total = sum(lst)
         return is_happy(total)
 
+
+#solution 2
+def sq_numbers(n):
+    return sum(int(i) ** 2 for i in str(n))
+
+
+def is_happy(n):
+    if len(str(n)) == 1 and n != 1:
+        return False
+    if n == 1:
+        return True
+
+    slow = sq_numbers(n)
+    fast = sq_numbers(sq_numbers(n))
+    while slow != fast:
+        if slow == 1 or fast == 1:
+            return True
+
+        slow = sq_numbers(slow)
+        fast = sq_numbers(fast)
+    return slow == 1
